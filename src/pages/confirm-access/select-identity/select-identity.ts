cdfb6ec5d985131d0b1822de720e3e59f44bdf46
samuelsan95 <sanchezlopezsamuel@gmail.com>
2019-09-05 17:25:18 +0200

    add header and clean ts

--- src/pages/confirm-access/select-identity/select-identity.ts
+++ src/pages/confirm-access/select-identity/select-identity.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ViewController, NavParams, ModalController } from 'ionic-angular';
+import { ViewController, NavParams, ModalController, NavController } from 'ionic-angular';
 import { ToastService } from '../../../services/toast-service';
 import { IdentitySecuredStorageService } from '../../../services/securedStorage.service';
 import { LoadingService } from '../../../services/loading-service';
@@ -11,32 +11,23 @@ import { LoadingService } from '../../../services/loading-service';
 })
 export class SelectIdentity {
 
-    public dataNumberAccess: number;
-    public isPresentationRequest: boolean;
-    public issName: string;
-
     private readonly CREDENTIAL_PREFIX = "cred_";
     private readonly PRESENTATION_PREFIX = "present_";
 
     private identitySelected: Array<number> = [];
     private credentials: Array<any>;
 
+    public searchTerm: string;
+
     constructor(
-        public viewCtrl: ViewController,
         public navParams: NavParams,
-        public modalCtrl: ModalController,
-        public toastCtrl: ToastService,
-        private securedStrg: IdentitySecuredStorageService,
-        private loadingSrv: LoadingService
+        public navCtrl: NavController
     ) {
-        this.dataNumberAccess = this.navParams.get("dataNumberAccess");
-        this.issName = this.navParams.get("issName");
         this.credentials = this.navParams.get("credentials");
         console.log('Credentials ', this.credentials)
         if (!this.credentials || this.credentials.length === 0) {
             this.credentials = [{"@context":"JWT","levelOfAssurance":2,"Carnet de Estudiante":"Pedro Pérez López","issuer":"SERVICE PROVIDER"}]
         }
-        this.isPresentationRequest = this.navParams.get("isPresentationRequest");
     }
 
     onStarClass(items: any, index: number, e: any) {
@@ -45,95 +36,12 @@ export class SelectIdentity {
         }
     }
 
-    public manageCredentials() {
-        if (this.isPresentationRequest) {
-            this.sendPresentation();
-        } else {
-            this.saveCredentials();
-        }
+    onSearch(event?: any) {
+        console.log('Event ', event);
+        console.log('searchTerm', this.searchTerm);
     }
-
-    private sendPresentation() {
-        let securedCredentials;
-
-        let credentialPromises = this.identitySelected.map((element) => {
-            let index = element;
-            return this.securedStrg.getJSON(this.CREDENTIAL_PREFIX + this.credentials[index]["field_name"]);
-        });
-
-        Promise.all(credentialPromises)
-            .then((result) => {
-                console.log(result);
-                securedCredentials = result;
-            });
-    }
-
-    private saveCredentials() {
-        if (this.identitySelected.length > 0) {
-            console.log('Sending Credentials');
-            this.showLoading();
-
-            let credentialPromises = this.identitySelected.map((element) => {
-                let index = element;
-                let credentialKeys = Object.getOwnPropertyNames(this.credentials[index]);
-
-                let hasKey;
-                let currentCredentialKey = this.CREDENTIAL_PREFIX + credentialKeys[2];
-                let currentCredentialValue;
-
-                let finalCredential = this.credentials[index];
-                finalCredential.issuer = this.issName;
-
-                let credentialPromise = this.securedStrg.hasKey(currentCredentialKey)
-                    .then(result => {
-                        hasKey = result;
-                        if (result) {
-                            return this.securedStrg.getJSON(currentCredentialKey);
-                        } else {
-                            return this.securedStrg.setJSON(currentCredentialKey, finalCredential);
-                        }
-                    }).then(result => {
-                        if (hasKey) {
-                            currentCredentialValue = result[credentialKeys[2]];
-                            if (this.credentials[index][credentialKeys[2]] !== currentCredentialValue) {
-                                return this.securedStrg.setJSON(currentCredentialKey + "_" + Math.random(), finalCredential);
-                            }
-                        } else {
-                            return Promise.resolve();
-                        }
-                    });
-                return credentialPromise;
-
-            });
-
-            //TODO: Confirm credential reception on blockchain
-
-            Promise.all(credentialPromises)
-                .then(() => {
-                    this.showSucces();
-                    this.viewCtrl.dismiss();
-                });
-
-        } else {
-            this.toastCtrl.presentToast("Por favor seleccione al menos una credential para enviar", 3000);
-        }
-    }
-
-    public handleIdentitySelect(identitySelect: any) {
-        if (identitySelect && identitySelect.value) {
-            this.identitySelected.push(identitySelect.id);
-        } else {
-            this.identitySelected = this.identitySelected.filter(identity => (identity !== identitySelect.id));
-        }
-
-        console.log(this.identitySelected);
-    }
-
-    public showLoading() {
-        this.loadingSrv.showModal();
-    }
-
-    public showSucces() {
-        this.loadingSrv.updateModalState();
+    
+    cancel() {
+        this.navCtrl.pop();
     }
 }
